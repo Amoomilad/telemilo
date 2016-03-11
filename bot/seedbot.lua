d5c97e0e3f2a261f809c52f4046fb5c1891c6626
Amoomilad <Amoomiladi@gmail.com>
2016-03-11 23:51:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {89159030,tonumber(our_id)},--Sudo users
+    sudo_users = {89159030,172249322,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
